c7b9565cb0639b89c412614f4e7591f7872a6ee8
zahybusiness <zahy.business@gmail.com>
2022-05-18 06:00:20 +0300

    create dashboard page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,19 @@ module.exports = {
       colors:{
         'gray-750':'#3f495a',
         'gray-850':'#222733',
+        'blue-650':'#206bc4',
       },
       spacing: {
         '14':'3.5rem',
         '22':'5.5rem',
         '72':'18rem'
       },
-      width:{},
-      fontSize:{}
+      //width:{},
+      //fontSize:{},
+      fontFamily:{
+        tajawal: ["Tajawal", "sans-serif"],
+      }
+
     },
     maxWidth: {
       '8xl': '82.5rem',
